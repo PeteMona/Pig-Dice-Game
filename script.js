0841961679b7a877a0a7eb1dd02b2ba6b168a508
PLM-Work <pietrosito@hotmail.com>
2024-01-01 14:51:41 +0100

    Added dice functionality

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 'use strict';
 
-//selecting the score element
+//selecting the score element and selecting the button element
 const score0El = document.querySelector('#score--1');
 const score1El = document.getElementById('score--0');
-
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 //selecting the dice element
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. Generating a random dice roll
+  const dice = Math.floor(Math.random() * 6) + 1;
+
+  //2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
