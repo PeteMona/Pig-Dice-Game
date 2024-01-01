ddb37e0685634b765e2d5ab26cb12a032b516739
PLM-Work <pietrosito@hotmail.com>
2024-01-01 15:14:55 +0100

    Added check for rolled dice and player switch

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 'use strict';
 
 //selecting the score element and selecting the button element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--1');
 const score1El = document.getElementById('score--0');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +17,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+//starting conditions
+let currentScore = 0;
+let activePlayer = 0;
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //1. Generating a random dice roll
@@ -20,4 +29,18 @@ btnRoll.addEventListener('click', function () {
   //2. Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  //3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+  }
 });
