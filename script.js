589ac4c5d417a1df5d633594888d5ff7f5fa4a3b
PLM-Work <pietrosito@hotmail.com>
2024-01-01 14:38:15 +0100

    Initial logic code in javaScript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//selecting the score element
+const score0El = document.querySelector('#score--1');
+const score1El = document.getElementById('score--0');
+
+const diceEl = document.querySelector('.dice');
+//selecting the dice element
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
